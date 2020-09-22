426039f4689500e5e425923633db720f4d1dbf74
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-09-23 03:11:59 +0700

    Initial commit

---
+++ for_stepik.cpp
@@ -0,0 +1,61 @@
+﻿// for_stepik.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{                   // LESS1:A7df892WDDFNeogoodjob
+    /*float pi;
+    //float f = 3.1415926;
+    int N = 512;
+    double res = -0.75;
+    printf("%.5d\n", N);
+    //printf("%5.0f\n", f);
+    printf("%5.2f\n", res);
+    scanf("%f", &pi);
+    char sym = '2';*/ // 1n6dQQjenwnsOutp4reveREnDof
+    //int rub = 0;
+  //  1 / 0;
+//    0 / 0;
+//    1.0 / 0.0;
+//    0.0 / 0.0;
+   // printf("%.13lf", rubles);
+    // 
+    //int diameter = 113, circumference = 355;
+   // printf("pi = %.5lf", (float)circumference / (float)diameter);
+
+    /*double angle_degrees = 0;
+    scanf("%lf", &angle_degrees);
+    printf("%.2lf", (angle_degrees * 3.1415926) / 180);
+    */
+    /*int x_1 = 0, x_2 = 0;
+    scanf("%d %d", &x_1, &x_2);
+    printf("%.0lf", fabs(x_1 - x_2));*/
+    /*double left_lenght_of_triangle = 0, right_lenght_of_triangle = 0, angle_of_triangle = 0;
+    scanf("%lf %lf %lf", &left_lenght_of_triangle, &right_lenght_of_triangle, &angle_of_triangle);
+    printf("%.2lf", left_lenght_of_triangle * right_lenght_of_triangle * 0.5 * sin((angle_of_triangle * 3.1415926) / 180)); */
+    int number_of_bits = 0, number_of_numbers = 0;
+    scanf("%d", &number_of_bits);
+    for (int i = 1; i <= number_of_bits; i++)
+    {
+        if (i == 1)
+        {
+            number_of_numbers = 1;
+        }
+        number_of_numbers *= 2;
+    }
+    printf("%d", number_of_numbers);
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
